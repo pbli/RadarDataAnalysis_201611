5565ef258791bdc323b6dbc81cfbf42967a521e1
Pengbo Li <pli3@unl.edu>
2016-12-13 17:02:33 -0600

    li

--- PlotPosVelErr_XY.m
+++ PlotPosVelErr_XY.m
@@ -1,92 +1,66 @@
 function [  ] = PlotPosVelErr_XY(refPV_XY,radPV_XY,errPV_XY,t)
 
-% make sure all plots in same scale for comparision
-radMinP=min( min(radPV_XY(:,1)), min(radPV_XY(:,2)));
-refMinP=min(min(refPV_XY(:,1)), min(refPV_XY(:,2)) );
-Pmin=min(radMinP,refMinP)-5;
-
-radMaxP=max(max(radPV_XY(:,1)),max(radPV_XY(:,2)));
-refMaxP=max(max(refPV_XY(:,1)),max(refPV_XY(:,2)));
-Pmax=max(radMaxP,refMaxP)+5;
-
-radMinV=min( min(radPV_XY(:,3)), min(radPV_XY(:,4)));
-refMinV=min(min(refPV_XY(:,3)), min(refPV_XY(:,4)) );
-Vmin=min(radMinV,refMinV)-0.5;
-
-radMaxV=max(max(radPV_XY(:,3)),max(radPV_XY(:,4)));
-refMaxV=max(max(refPV_XY(:,3)),max(refPV_XY(:,4)));
-Vmax=max(radMaxV,refMaxV)+0.5;
-
-ePmin=min(min(errPV_XY(:,1)),min(errPV_XY(:,2)));
-ePmax=max(max(errPV_XY(:,1)),max(errPV_XY(:,2)));
-
-eVmin=min(min(errPV_XY(:,3)),min(errPV_XY(:,4)));
-eVmax=max(max(errPV_XY(:,3)),max(errPV_XY(:,4)));
-
-
 for i=1:4
-    FigH = figure('Position', [200, 200, 1200, 600]);
-    subplot(1,2,1)
-    plot(t,radPV_XY(:,i),'b','LineWidth',3)
+    FigH = figure;
+    plot(t,refPV_XY(:,i),'r','LineWidth',3) 
     hold on
-    plot(t,refPV_XY(:,i),'r','LineWidth',1.5)
-    axis square;
-    set(gca,'FontSize',15)
+    plot(t,radPV_XY(:,i),'b','LineWidth',1.5)
+    set(gca,'FontSize',13)
     grid on
-    legend('rad','ref')
-    switch i
-        case 1
-            title('Position X')
-            ylabel('m')
-            ylim([Pmin Pmax])        
-            legend('rad','ref' ,'Location','SouthEast')
-        case 2
-            title('Position Y')
-            ylabel('m')
-            ylim([Pmin Pmax])
-        case 3
-            title('Velocity X')
-            ylim([Vmin Vmax])
-            ylabel('m/s')
-        case 4
-            title('Velocity Y')
-            ylim([Vmin Vmax])
-            ylabel('m/s')
-    end
+    legend('ref','rad')    
     xlim([0 max(t)+3] )% make sure the legend will not cover the plot
     xlabel('t(s)')
-    subplot(1,2,2)
-    bar(t,errPV_XY(:,i))
-    set(gca,'FontSize',15)
-    xlim([0 max(t)+3])
-    xlabel('t(s)')
-    grid on
-    axis square;
     set(gcf,'PaperType','usletter', ...
         'paperOrientation', 'landscape', ...
         'paperunits','CENTIMETERS', ...
-        'PaperPosition',[0, 4, 28, 14]);
+        'PaperPosition',[-1, 0, 29, 22]);
+    switch i
+        case 1
+            title('Position X')
+            ylabel('m') 
+            legend('rad','ref' ,'Location','SouthEast')
+            print(FigH,'-dpdf','Position on X axis.pdf')
+        case 2
+            title('Position Y')
+            ylabel('m')            
+            print(FigH,'-dpdf','Position on Y axis.pdf')
+        case 3
+            title('Velocity X')
+            ylabel('m/s')            
+            print(FigH,'-dpdf','Velocity on X axis.pdf')
+        case 4
+            title('Velocity Y')
+            ylabel('m/s')            
+            print(FigH,'-dpdf','Velocity on Y axis.pdf')
+    end
+
+    FigH = figure;
+    bar(t,errPV_XY(:,i),'b')
+    set(gca,'FontSize',13)
+    xlim([0 max(t)+3])
+    xlabel('t(s)')
+    grid on
+    set(gcf,'PaperType','usletter', ...
+        'paperOrientation', 'landscape', ...
+        'paperunits','CENTIMETERS', ...
+        'PaperPosition',[-1, 0, 29, 22]);
     switch i
         case 1
             title('Position error X')
             ylabel('m')
-            ylim([ePmin ePmax])
-            print(FigH,'-dpdf','X position and error.pdf')
+            print(FigH,'-dpdf','Possition error on X axis.pdf')
         case 2
             title('Position error Y')
             ylabel('m')
-            ylim([ePmin ePmax])
-            print(FigH,'-dpdf','Y position and error.pdf')            
+            print(FigH,'-dpdf','Position error on Y axis.pdf')            
         case 3
             title('Velocity error X')
-            ylabel('m/s')            
-            ylim([eVmin eVmax])
-            print(FigH,'-dpdf','X velocity and error.pdf')            
+            ylabel('m/s')
+            print(FigH,'-dpdf','Velocity error on X axis.pdf')            
         case 4
             title('Velocity error Y')
             ylabel('m/s')
-            ylim([eVmin eVmax])
-            print(FigH,'-dpdf','Y velocity and error.pdf')
+            print(FigH,'-dpdf','Velocity error on Y axis.pdf')
             
     end
 end
--- PlotTrajectory.m
+++ PlotTrajectory.m
@@ -1,23 +1,18 @@
 function [  ] =PlotTrajectory( radPV_XY,refPV_XY )
-FigH = figure('Position', [200, 200, 600, 600]);
+ FigH = figure;
 plot(refPV_XY(:,1),refPV_XY(:,2),'r','LineWidth',2)
 hold on
 plot(radPV_XY(:,1),radPV_XY(:,2),'b','LineWidth',2)
-minRange=min(min(radPV_XY(:,1)),min(radPV_XY(:,2)))-5;
-maxRange=max(max(radPV_XY(:,1)),max(radPV_XY(:,2)))+5;
-xlim([minRange maxRange ]);%square plot space
-ylim([minRange maxRange ]);
-set(gca,'FontSize',15)
-legend('Reference trajectory','Radar measured trajectory');
-title('trajectory plot')
-axis square;
+set(gca,'FontSize',13)
+legend('Reference trajectory','Radar measured trajectory','location','South');
+title('Trajectory plot')
 xlabel('X (m)')
 ylabel('Y (m)')
 grid on
 set(gcf,'PaperType','usletter', ...
          'paperOrientation', 'landscape', ...
          'paperunits','CENTIMETERS',...         
-         'PaperPosition',[6, 4, 14, 14]);
+         'PaperPosition',[-1, 0, 29, 22]);
 print(FigH,'-dpdf','Trajectory.pdf')
 
 end
